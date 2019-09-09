1ac7a7c2cd16588bd696f9e2f84be0637dfbd0af
Raul <rauldavid45@outlook.com>
2019-09-09 16:10:17 -0500

    remoteMethods post

--- common/models/city.js
+++ common/models/city.js
@@ -2,4 +2,40 @@
 
 module.exports = function(City) {
     City.validatesUniquenessOf('nombre');
+    City.status = function(id, cb) {
+        //  var currentDate = new Date();
+        //  var currentHour = currentDate.getHours();
+        //  console.log(currentHour, id);
+        //  var OPEN_HOUR = 6;
+        //  var CLOSE_HOUR = 20;
+        //  console.log('Current hour is %d', currentHour);
+        //  var response;
+        //  if (currentHour >= OPEN_HOUR && currentHour < CLOSE_HOUR) {
+        //      response = `${id}`;
+        //  } else {
+        //      response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
+        // }
+        City.findOne({ where: { id: id } }, function(err, dog) {
+            cb(null, dog);
+        });
+
+        // cb(null, response);
+    };
+    City.remoteMethod(
+        'status', {
+            http: {
+                path: '/:id/status',
+                verb: 'get'
+            },
+            returns: {
+                arg: 'status',
+                type: 'array'
+            },
+            accepts: {
+                arg: 'id',
+                type: 'string'
+            }
+        }
+    );
+    ///comienza otro metodo
 };
--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,83 @@
 'use strict';
 
 module.exports = function(Usuario) {
-    Usuario.validatesFormatOf('email', {with: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/});
+    Usuario.validatesFormatOf('email', { with: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ });
+    Usuario.create = function(req, cb) {
+        console.log(req);
+
+
+    }
+    Usuario.updachon = function(data, cb) {
+        let nombre = data.nombre;
+        let email = data.email;
+        let password = data.password;
+
+        const newUser = { nombre, email, password };
+        console.log(newUser);
+        Usuario.create(newUser, function(err, usuario) {
+            if (err) {
+                return cb(err);
+            } else {
+
+                return cb(null, usuario);
+
+            }
+        });
+    }
+    Usuario.remove = function(id, cb) {
+        app.Models.Usuario.remove({ id: id }, - function(err, usuario) {
+            if (err) {
+                return cb(err);
+            } else {
+                return cb(usuario)
+            }
+        });
+    }
+
+    Usuario.remoteMethod(
+        'updachon', {
+            http: {
+                path: '/updachon/:id',
+                verb: 'POST'
+            },
+            returns: {
+                arg: 'estado',
+                type: 'array'
+            },
+            accepts: [
+                { arg: 'data', type: 'object', http: { source: 'body' } }
+
+            ]
+        });
+    Usuario.remoteMethod(
+        'create', {
+            http: {
+                path: '/crear',
+                verb: 'POST'
+            },
+            returns: {
+                arg: 'estado',
+                type: 'array'
+            },
+            accepts: [
+                { arg: 'req', type: 'object', 'http': { source: 'req' } },
+
+            ]
+
+
+        });
+    Usuario.remoteMethod(
+        'remove', {
+            http: {
+                path: '/deleton',
+                verb: 'GET'
+            },
+            returns: {
+                arg: 'estado',
+                type: 'array'
+            },
+            accepts: [
+                { arg: 'id', type: 'object' }
+            ]
+        })
 };
